f68eb1c5c1f93220f1aba0ef6623d1c2b54c5144
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-07 14:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,11 +26,9 @@ function makeInt(n){
   return parseInt(n)
 }
 
-  function makeInt(n, 10){
-    return parseInt(n, 10)
-  }
-
-
+  it('assumes base 10', function() {
+    expect(makeInt('0x2328')).toEqual(0)
+  })
 
   it('returns NaN as appropriate', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
